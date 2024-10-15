d6097fe80de175158074ca4f00239e9164de7919
glauzinha-exe <glauzinnha.exe23@gmail.com>
2024-10-15 16:18:38 -0300

    adicao da troca de tema,titulo do aplicativo ,entrada de marca e modelo

--- MauiAppMeuCombustivel/MainPage.xaml.cs
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -1,13 +1,27 @@
-﻿namespace MauiAppMeuCombustivel
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppMeuCombustivel
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+        //evento do botao para mudar o tema
+        private void MudarTema(object sender, EventArgs e)
+        {
+            if (Application.Current.UserAppTheme == AppTheme.Light)
+            {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+            }
+            else
+            {
+                Application.Current.UserAppTheme = AppTheme.Light;
+            }
+        }
         private void Button_Clicked(object sender, EventArgs e)
         {
             try
@@ -15,19 +29,29 @@
                 Double etanol = Convert.ToDouble(txt_etanol.Text);
                 Double gasolina = Convert.ToDouble(txt_gasolina.Text);
                 string msg = "";
-                if(etanol <= (gasolina * 0.7))
+
+                //adicao das variaveis modelo e marca
+                string modelo = txt_modelo.Text;
+                string marca = txt_marca.Text;
+
+
+                if (etanol <= (gasolina * 0.7))
                 {
-                    msg = "O etanol está compensando.";
-                }else
-                {
-                    msg = "A gasolina está compensando.";
+                    //alteracao no retorno da msg,concatenando as strings
+                    msg = "O etanol está compensando para o seu " + marca + " " + modelo;
                 }
+                else
+                {
+                    msg = "A gasolina está compensando para o seu " + marca + " " + modelo;
+                }
+
+
                 DisplayAlert("Calculado", msg, "OK");
             }
             catch (Exception ex)
             {
-                DisplayAlert("ops", ex.Message, "fechar");
+                DisplayAlert("Ops", ex.Message, "Fechar");
             }
-        }//Fecha metodo
-    }//fecha class
-}//fecha namespace
+        } // Fecha método
+    } // Fecha classe
+} // Fecha namespace
